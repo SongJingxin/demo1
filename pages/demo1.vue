28331fdb1aa98f84ee8e15ea292cb45b10b2c437
SongJingxin <1944981402@qq.com>
2021-07-08 11:39:47 +0800

    demo1
    demo1测试

---
+++ pages/demo1.vue
@@ -0,0 +1,20 @@
+<!--  -->
+<template>
+  <div class="">{{ title }}</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "demo1",
+    };
+  },
+  components: {},
+  computed: {},
+  watch: {},
+  methods: {},
+};
+</script>
+<style lang='' scoped>
+</style>
